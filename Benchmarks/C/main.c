d0261e6428a4869a4c3817b0d554116e2a681843
Jakub Piotrowski <piotrowski.jakub72@gmail.com>
2020-05-26 22:12:26 +0200

    C - add gaussian elimination

--- Benchmarks/C/main.c
+++ Benchmarks/C/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <string.h>
 //#include <windows.h>
-
+#include <math.h>
 unsigned  long long int AckermanFunction(unsigned  long long int m, unsigned  long long int n){
     if (m == 0)
         return (n+1);
@@ -29,38 +29,58 @@ long long unsigned int FibonacciSequence(int n){
 }
 void SieveOfEratosthenes(int n)
 {
-    // Create a boolean array "prime[0..n]" and initialize
-    // all entries it as true. A value in prime[i] will
-    // finally be false if i is Not a prime, else true.
     bool prime[n+1];
     memset(prime, true, sizeof(prime));
 
     for (int p=2; p*p<=n; p++)
     {
-        // If prime[p] is not changed, then it is a prime
         if (prime[p] == true)
         {
-            // Update all multiples of p greater than or
-            // equal to the square of it
-            // numbers which are multiple of p and are
-            // less than p^2 are already been marked.
             for (int i=p*p; i<=n; i += p)
                 prime[i] = false;
         }
     }
-
-    // Print all prime numbers
     for (int p=2; p<=n; p++)
         if (prime[p])
             printf("%d ", p);
 }
+bool gauss (  double ** AB, double * X )
+{
+    const double eps = 1e-12; // stała przybliżenia zera
+    int i, j, k, n = 5;
+    double m, s;
 
+    // eliminacja współczynników
+    for( i = 0; i < n - 1; i++ )
+    {
+        for( j = i + 1; j < n; j++ )
+        {
+            if( fabs ( AB [ i ][ i ] ) < eps ) return false;
+            m = -AB [ j ][ i ] / AB [ i ][ i ];
+            for( k = i + 1; k <= n; k++ )
+                AB [ j ][ k ] += m * AB [ i ][ k ];
+        }
+    }
+    // wyliczanie niewiadomych
+
+    for( i = n - 1; i >= 0; i-- )
+    {
+        s = AB [ i ][ n ];
+        for( j = n - 1; j >= i + 1; j-- )
+            s -= AB [ i ][ j ] * X [ j ];
+        if( fabs ( AB [ i ][ i ] ) < eps ) return false;
+        X [ i ] = s / AB [ i ][ i ];
+    }
+    return true;
+}
 
 int main() {
     clock_t start = clock();
     printf("%llu\n", AckermanFunction(3,13));
+//    SieveOfEratosthenes(50);
     clock_t end = clock();
     printf("%f\n", (float)(end - start) / CLOCKS_PER_SEC);
+    scanf("%d");
 //    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
 
 //    LARGE_INTEGER frequency;        // ticks per second
